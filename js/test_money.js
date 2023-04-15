35bcf8602dd5ffc2cccbc9bad7cd0d8ee85b4ece
MartinMorning <bendashu1920@outlook.com>
2023-04-15 19:32:53 +0800

    feat: division and multiplication features done

--- js/test_money.js
+++ js/test_money.js
@@ -1,15 +1,22 @@
 const assert = require('assert');
 
-class Dollar {
-    constructor(amount) {
+class Money {
+    constructor(amount, currency) {
         this.amount = amount;
+        this.currency = currency;
     }
 
     times(multiplier) {
-        return new Dollar(this.amount * multiplier);
+        return new Money(this.amount * multiplier,
+                        this.currency);
+    }
+
+    divide(divisor) {
+        return new Money(this.amount / divisor,
+                        this.currency);
     }
 }
 
-let fiver = new Dollar(5);
-let tenner = fiver.times(2);
-assert.strictEqual(tenner.amount, 10);
+let originalMoney = new Money(4002, "KRW")
+let expectedMoneyAfterDivision = new Money(1000.5, "KRW")
+assert.deepStrictEqual(originalMoney.divide(4), expectedMoneyAfterDivision)
--- py/test_money.py
+++ py/test_money.py
@@ -1,17 +1,30 @@
 import unittest
 
-class Dollar:
-    def __init__(self, amount) -> None:
+class Money:
+    def __init__(self, amount, currency) -> None:
         self.amount = amount
+        self.currency = currency
+
+    def __eq__(self, other: object) -> bool:
+        return self.amount == other.amount and self.currency == other.currency
 
     def times(self, multiplier):
-        return Dollar(self.amount * multiplier)
+        return Money(self.amount * multiplier, self.currency)
+    
+    def divide(self, divisor):
+        return Money(self.amount / divisor, self.currency)
 
 class TestMoney(unittest.TestCase):
-    def testMultiplication(self):
-        fiver = Dollar(5)
-        tenner = fiver.times(2)
-        self.assertEqual(10, tenner.amount)
+    def testMultiplicationInDollars(self):
+        fiver = Money(5, "USD")
+        tenner = Money(10, "USD")
+        self.assertEqual(tenner, fiver.times(2))
+
+    def testDivision(self):
+        originalMoney = Money(4002, "KRW")
+        expectedMoneyAfterDivision = Money(1000.5, "KRW")
+        self.assertEqual(expectedMoneyAfterDivision,
+                        originalMoney.divide(4))
 
 if __name__ == '__main__':
     unittest.main()
