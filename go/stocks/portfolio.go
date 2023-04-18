473e5403602d6cf1f0cf557d2af0559e523f8b59
MartinMorning <bendashu1920@outlook.com>
2023-04-18 18:33:00 +0800

    feat: conversion of Money from EUR to USD

--- go/stocks/portfolio.go
+++ go/stocks/portfolio.go
@@ -2,6 +2,14 @@ package stocks
 
 type Portfolio []Money
 
+func convert(money Money, currency string) float64 {
+	eurToUsd := 1.2
+	if money.currency == currency {
+		return money.amount
+	}
+	return money.amount * eurToUsd
+}
+
 func (p Portfolio) Add(money Money) Portfolio {
 	p = append(p, money)
 	return p
@@ -10,7 +18,7 @@ func (p Portfolio) Add(money Money) Portfolio {
 func (p Portfolio) Evaluate(currency string) Money {
 	total := 0.0
 	for _, m := range p {
-		total = total + m.amount
+		total = total + convert(m, currency)
 	}
 	return Money{amount: total, currency: currency}
 }
--- js/portfolio.js
+++ js/portfolio.js
@@ -5,13 +5,21 @@ class Portfolio {
         this.moneys = [];
     }
 
+    convert(money, currency) {
+        let eurToUsd = 1.2;
+        if(money.currency === currency) {
+            return money.amount;
+        }
+        return money.amount * eurToUsd;
+    }
+
     add(...moneys) {
         this.moneys = this.moneys.concat(moneys);
     }
 
     evaluate(currency) {
         let total = this.moneys.reduce((sum, money) => {
-            return sum + money.amount;
+            return sum + this.convert(money, currency);
         }, 0);
         return new Money(total, currency);
     }
