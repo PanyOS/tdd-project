330bec62cec033504cfedec6c39d5be3e68b886f
MartinMorning <bendashu1920@outlook.com>
2023-04-18 23:34:08 +0800

    feat: conversion between currencies with defined exchange rates

--- js/test_money.js
+++ js/test_money.js
@@ -35,6 +35,15 @@ class MoneyTest {
                                 expectedValue);
     }
 
+    testAdditionOfDollarsAndWons() {
+        let oneDollar = new Money(1, "USD");
+        let elevenHundredWon = new Money(1100, "KRW");
+        let portfolio = new Portfolio();
+        portfolio.add(oneDollar, elevenHundredWon);
+        let expectedValue = new Money(2200, "KRW");
+        assert.deepStrictEqual(portfolio.evaluate("KRW"), expectedValue);
+    }
+
     getAllTestMethods() {
         let moneyPrototype = MoneyTest.prototype;
         let allProps = Object.getOwnPropertyNames(moneyPrototype);
